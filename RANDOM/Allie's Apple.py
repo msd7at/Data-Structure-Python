c083373ac474260c5a3dbb0293a4a8b7df85460b
Anurag Tiwari <52295974+msd7at@users.noreply.github.com>
2020-12-22 18:28:09 +0530

    Update Allie's Apple.py

--- RANDOM/Allie's Apple.py
+++ RANDOM/Allie's Apple.py
@@ -1,12 +1,10 @@
 n=int(input())
-i=1
-ans=0
-while True:
-    for j in range(i+1):
-        x=i*10 +j
-        ans=ans+(i+j)*4
+def res(input1):
+    xa=0
+    s=0
+    while input1 > xa:
+        s+=1
+        xa=xa+12*(s*s)
+    return s*8
+print(res(n))
     
-    if ans>=n:
-        print(i*8)
-        break
-    i+=1
