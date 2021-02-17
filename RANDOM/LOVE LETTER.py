e21b7f185bd5df9b7f38c5f691f6792f4097a8a8
Anurag Tiwari <52295974+msd7at@users.noreply.github.com>
2021-02-17 10:48:43 +0530

    Update LOVE LETTER.py

--- RANDOM/LOVE LETTER.py
+++ RANDOM/LOVE LETTER.py
@@ -1,17 +1,19 @@
-n=input()
-n=n.split()
-k=int(input())
-ans=[]
-for i in n:
-    l=len(i)
-    t=""
-    for j in range(l):
-        t+=i[(j+k)%l]
-    ans.append(t)
-jj=0
-print(n)
-print(ans)
-for i in range(len(n)):
-    if n[i]==ans[i]:
-        jj+=1
-print(jj)
+class UserMainCode(object):
+    @classmethod
+    def rotatedWords(cls ,input1, input2):
+        input1=input1.split()            
+        ans=[]
+        for i in input1:
+            l=len(i)
+            t=""
+            for j in range(l):
+                t+=i[(j+input2)%l]
+            ans.append(t)
+        jj=0
+
+        for i in range(len(input1)):
+            if input1[i]==ans[i]:
+                jj+=1
+        return jj
+obj=UserMainCode()
+print(obj.rotatedWords('adaada',3))
