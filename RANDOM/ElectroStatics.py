32f8e90bc57a99a6ec0e202c7a313cdc5ba1bfa3
Anurag Tiwari <52295974+msd7at@users.noreply.github.com>
2021-02-17 10:49:06 +0530

    Update ElectroStatics.py

--- RANDOM/ElectroStatics.py
+++ RANDOM/ElectroStatics.py
@@ -1,12 +1,16 @@
-va=list(map(int,input().split()))
-ch=input()
-n=int(input())
-p=0
-ne=0
-for i in range(n):
-    if ch[i]=="P":
-        p+=va[i]
-    else:
-        ne+=va[i]
-print(va,ch,p,ne)
-print(abs(n-p)*100)
+class UserMainCode(object):
+    @classmethod
+    def findmaximum(cls,input1 ,input2,input3):
+        p=0
+        ne=0
+        for i in range(input3):
+            if input2[i]=="P":
+                p+=input1[i]
+            else:
+                ne+=input1[i]
+        
+        return(abs(ne-p)*100)
+   
+
+obj=UserMainCode()
+print(obj.findmaximum([3,2],'PN',2))
